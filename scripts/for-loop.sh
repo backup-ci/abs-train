353867cfb96e2745fb52e2ccc736a894bda3de43
Jarson Fang <fanghuangcai@163.com>
2016-08-08 17:13:50 +0800

    update slides and add corresponding scripts

---
+++ scripts/for-loop.sh
@@ -0,0 +1,8 @@
+for arg in "$var1" "$var2" "$var3" ... "$varN"
+# In pass 1 of the loop, arg = $var1
+# In pass 2 of the loop, arg = $var2
+# In pass 3 of the loop, arg = $var3
+# ...
+# In pass N of the loop, arg = $varN
+
+# Arguments in [list] quoted to prevent possible word splitting.
