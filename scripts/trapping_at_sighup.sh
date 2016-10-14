e50b1b127c28dc6c8c3b89c3eaeebbdb83817d1b
Jarson Fang <jarson.fang@gmail.com>
2016-10-14 16:37:45 +0800

    update trapping_at_sighup.sh

--- scripts/trapping_at_sighup.sh
+++ scripts/trapping_at_sighup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-BrdNo=132
-User=$USER
-
 trap_func()
 {
-	ipmitool -H 192.168.1.132 -I lanplus -U root -P Huawei12#$ sol deactivate
+	ipmitool -H 192.168.1.132 -I lanplus -U root -P 'Huawei12#$' sol deactivate
 	end_time=`date +%Y\ %m\ %d\ %H:%M`
 	echo "$BrdNo,$User,$start_time,$end_time" >> statistical.cvs
 }
 
-if [ ! -f statistical.cvs ]; then
-	echo "BrdNo,User,Start,End" > statistical.cvs
+BrdNo=132
+User=$USER
+
+if [ ! -f statistical.csv ]; then
+	echo "BrdNo,User,Start,End" > statistical.csv
 fi
 
 trap 'trap_func; exit 0' SIGHUP
+
 start_time=`date +%Y\ %m\ %d\ %H:%M`
-ipmitool -H 192.168.1.132 -I lanplus -U root -P Huawei12#$ sol deactivate
-if ipmitool -H 192.168.1.132 -I lanplus -U root -P Huawei12#$ sol activate; then
-	end_time=`date +%Y\ %m\ %d\ %H:%M`
-	echo "$BrdNo,$User,$start_time,$end_time" >> statistical.cvs
-fi
+ipmitool -H 192.168.1.132 -I lanplus -U root -P 'Huawei12#$' sol deactivate
+ipmitool -H 192.168.1.132 -I lanplus -U root -P 'Huawei12#$' sol activate;
+
+# ipmitool exit
+end_time=`date +%Y\ %m\ %d\ %H:%M`
+echo "$BrdNo,$User,$start_time,$end_time" >> statistical.csv
